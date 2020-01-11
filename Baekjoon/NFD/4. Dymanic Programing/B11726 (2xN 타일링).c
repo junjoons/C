ec7cbb6d493354f706b17e93357da497a4698f5f
publicJunjoons <schooljunyoung@gmail.com>
2020-01-11 19:15:39 +0900

    B11726

---
+++ Baekjoon/NFD/4. Dymanic Programing/B11726 (2xN 타일링).c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+
+	int tiling[1000] = { 1, 2, 3, 5, 8, 0 };
+	for (int i = 2; i < n; i++)
+		tiling[i] = (tiling[i - 2] + tiling[i - 1]) % 10007;
+
+	printf("%d", tiling[n-1]);
+	return 0;
+}
