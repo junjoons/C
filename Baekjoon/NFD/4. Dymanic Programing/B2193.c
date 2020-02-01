f2e7034b964610a99979d27df8384d85664f53b1
publicjunjoons <schooljunyoung@gmail.com>
2020-02-01 19:19:33 +0900

    20200201 CodingPlus +

--- Baekjoon/NFD/4. Dymanic Programing/B2193.c
+++ Baekjoon/NFD/4. Dymanic Programing/B2193.c
@@ -3,12 +3,15 @@
 
 int main()
 {
-	int dp[91][2] = { {0, 0}, {0, 1}, {1, 0}, {} };
+	int dp[91][2] = { {0, 0}, {0, 1}, {1, 0}, {1, 1}, };
 	int n;
+	scanf("%d", &n);
+
 	for (int i = 0; i <= n; i++)
 	{
-		for(int j = 0; j<)
+		
 	}
+
 	scanf("%d", &n);
 	return 0;
 }
