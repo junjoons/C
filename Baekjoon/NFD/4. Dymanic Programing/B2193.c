d19137229fa47f28723bcd3a7ecf6836f75fc3d7
publicjunjoons <schooljunyoung@gmail.com>
2020-02-08 12:18:19 +0900

    20200208 CodingPlus

--- Baekjoon/NFD/4. Dymanic Programing/B2193.c
+++ Baekjoon/NFD/4. Dymanic Programing/B2193.c
@@ -3,15 +3,18 @@
 
 int main()
 {
-	int dp[91][2] = { {0, 0}, {0, 1}, {1, 0}, {1, 1}, };
+	long long int dp[91][2] = { {0, 0}, {0, 1}, {1, 0}, {1, 1}, };
 	int n;
 	scanf("%d", &n);
 
-	for (int i = 0; i <= n; i++)
+	for (int i = 4; i <= n; i++)
 	{
-		
+		for (int j = 0; j < 2; j++)
+		{
+			dp[i][j] = dp[i - 2][j] + dp[i - 1][j];
+		}
 	}
-
-	scanf("%d", &n);
+	long long int data = dp[n][0] + dp[n][1];
+	printf("%lld", data);
 	return 0;
 }
---
+++ Baekjoon/NFD/4. Dymanic Programing/B9465_AT1.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+int main()
+{
+	int exenb; //¹®Á¦»ó t
+	int n;
+
+	scanf("%d", &exenb);
+
+	for (int i = 0; i < exenb; i++)
+	{
+		int dp[2][100000];
+
+		scanf("%d", &n);
+
+		for (int j = 0; j < 2; j++)
+		{
+			for (int k = 0; k < n; k++)
+			scanf("%d", &dp[j][k]);
+		}
+		for (int k = 0; k < 2; k++)
+		{
+			for (int j = 0; j< n; j++)
+			{
+				if (k == 0)
+				{
+					if (j == 0)
+					{
+						if (dp[k][j] > dp[k + 1][j] && dp[k][j] > dp[k][j + 1])
+							dp[k + 1][j] = dp[k][j + 1] = 0;
+						else
+							dp[k][j] = 0;
+					}
+					else if (j == n - 1)
+					{
+						if (dp[k][j] > dp[k +1][j] && dp[k][j] > dp[k][j - 1])
+							dp[k +1][j] = dp[k][j -1] = 0;
+						else
+							dp[k][j] = 0;
+					}
+					else
+					{
+						if (dp[k][j] > dp[k][j-1] && dp[k][j] > dp[k][j + 1] && dp[k][j]>dp[k+1][j])
+							dp[k][j+1] = dp[k][j - 1] = dp[k+1][j] = 0;
+						else
+							dp[k][j] = 0;
+					}
+				}
+				else if (k == 1)
+				{
+					if (j == 0)
+					{
+						if (dp[k][j] > dp[k -1][j] && dp[k][j] > dp[k][j +1])
+							dp[k - 1][j] = dp[k][j +1] = 0;
+						else
+							dp[k][j] = 0;
+					}
+					else if (j == n - 1)
+					{
+						if (dp[k][j] > dp[k - 1][j] && dp[k][j] > dp[k][j-1])
+							dp[k -1][j] = dp[k][j -1] = 0;
+						else
+							dp[k][j] = 0;
+					}
+					else
+					{
+						if (dp[k][j] > dp[k][j - 1] && dp[k][j] > dp[k][j + 1] && dp[k][j] > dp[k - 1][j])
+							dp[k][j + 1] = dp[k][j - 1] = dp[k - 1][j] = 0;
+						else
+							dp[k][j] = 0;
+					}
+				}
+			}
+		}
+		int sum = 0;
+		for (int k = 0; k < 2; k++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				sum = sum + dp[k][j];
+			}
+		}
+		printf("%d\n", sum);
+	}
+	return 0;
+}
